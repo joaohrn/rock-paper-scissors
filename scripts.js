4ec990096a1b7cd8222ee1417947669a71f90bae
joaohrn <joaohrn0@gmail.com>
2022-12-09 21:38:20 -0300

    Add playRound function

--- scripts.js
+++ scripts.js
@@ -8,3 +8,18 @@ function getComputerChoice() {
     return "scissors";
   }
 }
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+    return `It's a draw`;
+  } else if (
+    (playerSelection.toLowerCase() == `rock` &&
+      computerSelection == `scissors`) ||
+    (playerSelection.toLowerCase() == `scissors` &&
+      computerSelection == `paper`) ||
+    (playerSelection.toLowerCase() == `paper` && computerSelection == `rock`)
+  ) {
+    return `You win ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return `You lose ${computerSelection} beats ${playerSelection}`;
+  }
+}
